a71fe8867d7836783dc4fa6aee74e53b770802a0
BogdanEno <32198410+BogdanEno@users.noreply.github.com>
2017-10-30 03:50:05 +0300

    Add files via upload

---
+++ New wersion/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 fr1 = new Form1();
+            fr1.Show();
+            Hide();
+        }
+        private static string POST(string Url, string Data)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(Url);
+            req.Method = "POST";
+            req.Timeout = 100000;
+            req.ContentType = "application/x-www-form-urlencoded";
+            byte[] sentData = Encoding.GetEncoding(1251).GetBytes(Data);
+            req.ContentLength = sentData.Length;
+            System.IO.Stream sendStream = req.GetRequestStream();
+            sendStream.Write(sentData, 0, sentData.Length);
+            sendStream.Close();
+            System.Net.WebResponse res = req.GetResponse();
+            System.IO.Stream ReceiveStream = res.GetResponseStream();
+            System.IO.StreamReader sr = new System.IO.StreamReader(ReceiveStream, Encoding.UTF8);
+            //Кодировка указывается в зависимости от кодировки ответа сервера
+            Char[] read = new Char[256];
+            int count = sr.Read(read, 0, 256);
+            string Out = String.Empty;
+            while (count > 0)
+            {
+                String str = new String(read, 0, count);
+                Out += str;
+                count = sr.Read(read, 0, 256);
+            }
+            return Out;
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox3.Text = POST("http://www.zapomnika.zzz.com.ua/Lab4.php", textBox1.Text);
+        }
+    }
+}
